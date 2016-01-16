42d3d2ac7b069d900f30fbbfdbabcbe96567a701
dkmullen <dkmullen@gmail.com>
2016-01-16 10:56:11 -0500

    Completely refactored code acc to concerns. No comments yet.

--- cat.js
+++ cat.js
@@ -1,123 +1,96 @@
-var loadPage = function() {
+/** Model */
 
-/** Model */	
-	var myCats = [
+var model = {
+	currentCat: null,
+	myCats: [
 		{name: "BJ", pic: "pix/bj.jpg", counter: 0},
 		{name: "Kiri", pic: "pix/kiri.jpg", counter: 0},
 		{name: "Smudge", pic: "pix/smudge.jpg", counter: 0},
 		{name: "Thor", pic: "pix/thor.jpg", counter: 0},
 		{name: "Unknown", pic: "pix/unknown.jpg", counter: 0}
-	];
-	
+	]
+};	
 
 /** Octopus */
-	/** 
-	 *  Makes the menu of cats
-	 *  @function makeMenu
-	 */
-	var makeMenu = function() {
-		/** Pulls myCats.length out of the loop for better performance. */
-		var myCatsLen = myCats.length;
-		
-		/** Loops thru myCats creating a DOM element (a menu) including each cat.
-		 *  Adds an event listener w/c stores the clicked item in a closure,
-		 *  and calls displayCat to display the selected item on the page.
-	 	 */
-		for (var i = 0; i < myCatsLen; i++) {
-			var catNum = myCats[i];
+
+var octopus = {
+	
+	init: function() {
+		model.currentCat = model.myCats[Math.floor(Math.random() * model.myCats.length)];
+		catMenuView.init();
+		catView.init();
+	},
+	
+	getCurrentCat: function() {
+		return model.currentCat;
+	},
+	
+	getAllCats: function() {
+		return model.myCats;
+	},
+	
+	setCurrentCat: function(cat) {
+		model.currentCat = cat;
+	},
+	
+	incrementCounter: function() {
+		model.currentCat.counter++;
+		catView.render();
+	}
+};
+
+/** Display */
+
+var catMenuView = {
+	init: function() {
+		this.catListElem = document.getElementById('menu');
+		this.render();
+	},
+	
+	render: function() {
+		var cat, node, i;
+		var allCats = octopus.getAllCats();
+		this.catListElem.innerHTML = '';
+		for (i = 0; i < allCats.length; i++) {
+			cat = allCats[i];
 			
-			var node = document.createElement("BUTTON");
+			node = document.createElement("BUTTON");
 			node.id = i;
-			var textnode = document.createTextNode(myCats[i].name);
+			var textnode = document.createTextNode(allCats[i].name);
 			node.appendChild(textnode);
-			document.getElementById("menu").appendChild(node);
 			
 			node.addEventListener('click', (function(catCopy) {
 				return function() {
-					displayCat(catCopy);
-					displayCounter(catCopy);
+					octopus.setCurrentCat(catCopy);
+					catView.render(catCopy);
 				};
-			})(catNum));
-		};
-	};
-	
-	/** 
-	 *  Displays a random cat on page load and a selected cat when
-	 *  called by the event listener.
-	 *  @function displayCat
-	 */
-	var displayCat = function(n) {
-		document.getElementById("cat_name").innerHTML = n.name;
-		document.getElementById("cat_pic").innerHTML = '<img id =' + n.name + ' src="' + n.pic + '">';
-		/** Sets n as a global variable currentCat to be used to track clicks 
-		 *  on the cat currently being displayed.
-		*/
-		currentCat = n;
-	};
+			})(cat));
+			
+			this.catListElem.appendChild(node);
+		}
+	}
+};
 
-	/** 
-	 *  Displays the counter of the above on page load and a selected one when
-	 *  called by the event listener.
-	 *  @function displayCounter
-	 */	
-	var displayCounter = function(n) {
-		document.getElementById("counter").innerHTML = "Counter: " + n.counter;
-	};
+var catView = {
+	init: function() {
+		this.catElem = document.getElementById("cat");
+		this.catNameElem = document.getElementById("cat_name");
+		this.catImageElem = document.getElementById("cat_pic");
+		this.countElem = document.getElementById("counter");
+		
+		this.catImageElem.addEventListener('click', function() {
+			octopus.incrementCounter();
+		});
+		
+		this.render();
+	},
 	
-	/** 
-	 * A helper function to calculate random numbers between 0
-	 * (inclusive) and n (exclusive).
-	 * @function randNum 
-	 */
-	var randNum = function(n) {
-		return Math.floor(Math.random() * n);
-	};
-	
-	/** Picks a random cat to display on page load. */
-	var displayThisCat = myCats[randNum(5)];
-
-	var elem = document.getElementById("cat_pic");
-	elem.addEventListener('click', (function(picCopy) {
-		return function() {
-			/* Increments count of currently displayed cat, updates it 
-			 * in the myCats list, updates the counter display;
-			 */
-			currentCat.counter++;
-			displayCounter(currentCat);
-		};
-	})(elem));	
-	
-	var displayAdminArea = function() {
-		document.getElementById('admin-area').innerHTML = 
-			'<form>Name:<input type="text" name="name" value=' + currentCat.name + '><br>' +
-            'Image url:<input type="text" name="pic" value=' + currentCat.pic + '><br>' +
-			'Counter:<input type="text" name="counter" value=' + currentCat.counter + '><br><br>' +
-			'<button id="cancel">Cancel</button>' +
-			'<input type="submit" value="Submit"></form>';
-	};
-	
-	var adminButton = document.getElementById('admin');
-	adminButton.addEventListener('click', function() {
-		displayAdminArea();
-	});
-	
-	hideAdmin = function() {
-		//document.getElementById('admin-area').style.display = "none";
-		console.log("Hello");
-	};
-	
-	updateCats = function() {
-		console.log("Hello");
-	};
-
-/** Display */
-	/** calls makeMenu on page load */
-	makeMenu();
-	
-	/** calls displayCat and displayCounter on page load. */
-	displayCat(displayThisCat);
-	displayCounter(displayThisCat);
-}; 
-
-/** Calls loadPage on page load. */
-loadPage();	
+	render: function() {
+		var currentCat = octopus.getCurrentCat();
+		this.catNameElem.textContent = currentCat.name;
+		this.catImageElem.src = currentCat.pic;
+		this.countElem.textContent = currentCat.counter;
+		
+	}
+};
+octopus.init();
