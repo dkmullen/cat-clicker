49c57c1f04fb3b0ba34c654e3520afa772521d54
dkmullen <dkmullen@gmail.com>
2016-01-16 14:31:24 -0500

    Added a partially-working admin area, fixed wrong js call in html. Still need to made admin forms functional.

--- cat.js
+++ cat.js
@@ -1,4 +1,4 @@
-/** Model */
+/** Model - The data*/
 
 var model = {
 	currentCat: null,
@@ -11,55 +11,64 @@ var model = {
 	]
 };	
 
-/** Octopus */
+/** Octopus - to communicate between model and Display*/
 
 var octopus = {
 	
+	/** Picks a random cat, calls the inits in Display */
 	init: function() {
 		model.currentCat = model.myCats[Math.floor(Math.random() * model.myCats.length)];
 		catMenuView.init();
 		catView.init();
+		adminView.init();
 	},
 	
+	/** A @function to be called when currentCat is needed */
 	getCurrentCat: function() {
 		return model.currentCat;
 	},
 	
+	/** A @function to be called when the whole myCats object is needed */
 	getAllCats: function() {
 		return model.myCats;
 	},
 	
+	/** A @function to be called to set the current currentCat */
 	setCurrentCat: function(cat) {
 		model.currentCat = cat;
 	},
 	
+	/** A @function to be called when counter needed updated */
 	incrementCounter: function() {
 		model.currentCat.counter++;
 		catView.render();
 	}
 };
 
-/** Display */
+/** Display - Contains only code to be put on the screen */
 
 var catMenuView = {
+	/** A @function which sets a variable for menu element, calls render */
 	init: function() {
 		this.catListElem = document.getElementById('menu');
 		this.render();
 	},
 	
+	/** A @function that sets variables, gets the list of cats, builds the menu */
 	render: function() {
 		var cat, node, i;
 		var allCats = octopus.getAllCats();
-		this.catListElem.innerHTML = '';
 		for (i = 0; i < allCats.length; i++) {
 			cat = allCats[i];
 			
+			/** Creates buttons, adds text and an event listener */
 			node = document.createElement("BUTTON");
 			node.id = i;
 			var textnode = document.createTextNode(allCats[i].name);
 			node.appendChild(textnode);
 			
 			node.addEventListener('click', (function(catCopy) {
+				/** Saves a temp copy of the clicked button, renders the right cat */
 				return function() {
 					octopus.setCurrentCat(catCopy);
 					catView.render(catCopy);
@@ -72,6 +81,7 @@ var catMenuView = {
 };
 
 var catView = {
+	/** A @function to create the cat in the DOM */
 	init: function() {
 		this.catElem = document.getElementById("cat");
 		this.catNameElem = document.getElementById("cat_name");
@@ -85,6 +95,7 @@ var catView = {
 		this.render();
 	},
 	
+	/** A @function to render the above cat */
 	render: function() {
 		var currentCat = octopus.getCurrentCat();
 		this.catNameElem.textContent = currentCat.name;
@@ -93,4 +104,32 @@ var catView = {
 		
 	}
 };
+
+var adminView = {
+	/** A @function to create the admin ares; NOT rendered on page load */
+	init: function() {
+		this.adminElem = document.getElementById('admin-area');
+		this.adminButton = document.getElementById('admin-button');
+		
+		/** This is the only place where render is called */
+		this.adminButton.addEventListener('click', function() {
+			adminView.render();
+		});
+	},
+	
+	/** A @function to render the admin box on click of admin button */
+	render: function() {
+		var currentCat = octopus.getCurrentCat();
+		this.adminElem.innerHTML = 
+			'<div id="admin-box">' +
+			'Name:<input type="text" name="name" value="' + currentCat.name + '"><br>' +
+			'Img URL:<input type="text" name="pic" value="' + currentCat.pic + '"><br>' +
+			'Counter:<input type="text" name="counter" value="' + currentCat.counter + '"><br><br>' +
+			'<button id="cancel">Cancel</button>' +
+			'<button id="save">Save</button>' +
+			'</div>';
+	}
+};
+
+/** Loads the page. Octopus calls the inits which call the renders */
 octopus.init();
